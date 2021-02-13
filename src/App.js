dea62c3c34bceee9dc9f0bdf32a6062c030ced94
henokwel <hen.ok@live.com>
2021-02-13 23:56:03 +0100

    init files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
+
+  const toRoutes = [
+    { id: 1, title: "one" },
+    { id: 2, title: "two" },
+    { id: 3, title: "three" }
+  ]
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Switch  >
+
+        <Route to />
+      </Switch>
+
     </div>
   );
 }
