79eca439e6044856b6f1c0070a6e35a689a98665
henokwel <hen.ok@live.com>
2021-02-14 03:21:04 +0100

    404 added with miner css changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import { Link, Route, Switch, BrowserRouter as Router, useParams, NavLink } from 'react-router-dom';
 import scripts from './scripts';
+import click from './Assets/clicksvg.svg'
+import n404 from './Assets/404.png'
+
 
 function App() {
 
@@ -18,27 +21,32 @@ function App() {
       <div className="App">
 
         <nav>
-          
 
-          {
-            toRoutes.map(({ title, id }) => {
-              return <NavLink
-                exact
-                key={id}
-                activeClassName="selected"
-                to={`/${title}`}>
-                <h3>
-                  {title.toLocaleUpperCase()}
-                </h3>
-              </NavLink>
+          <h1>Chapter's</h1>
+          <div>
+            {
+              toRoutes.map(({ title, id }) => {
+                return <NavLink
+                  exact
+                  key={id}
+                  activeClassName="selected"
+                  to={`/${title}`}>
+                  <h3>
+                    {title.toLocaleUpperCase()}
+                  </h3>
+                </NavLink>
 
-            })
-          }
+              })
+            }
+          </div>
         </nav>
 
         <Switch>
 
           <Route path="/:id" children={<Child />} />
+          <Route path="/" children={<Home />} />
+          <Route path="*" children={<NotFounf />} />
+
         </Switch>
       </div>
     </Router>
@@ -51,10 +59,34 @@ export default App;
 function Child() {
   let { id } = useParams()
   const selected = scripts.filter(item => item.id === id)[0]
+console.log(selected);
 
   return (
     <div className="chap">
+      {selected !== undefined ? 
       <p>{selected.script}</p>
+      :
+      <NotFounf />
+      
+      }
+    </div>
+  )
+} 
+function Home() {
+
+  return (
+    <div className="home">
+      <img src={click} width="100%" alt="indicator" />
+    </div>
+  )
+}
+
+function NotFounf() {
+
+  return (
+    <div className="n404">
+      <img src={n404} width="100%" alt="404" />
+      <h3>Chapter not found</h3>
     </div>
   )
 }
